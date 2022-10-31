c326a0b24917004bd986bfdaa77d8c11966812c7
Christopher-Mata <Christophermataaldana@gmail.com>
2022-10-31 11:39:09 -0500

    Switched the bot to maven and added video playability via youtube

---
+++ iCalories/iCalories/Helpers/TimeFormatting.swift
@@ -0,0 +1,8 @@
+//
+//  TimeFormatting.swift
+//  iCalories
+//
+//  Created by Christopher Mata on 8/26/22.
+//
+
+import Foundation
---
+++ iCalories/iCalories/Views/EditFoodView.swift
@@ -0,0 +1,20 @@
+//
+//  EditFoodView.swift
+//  iCalories
+//
+//  Created by Christopher Mata on 8/26/22.
+//
+
+import SwiftUI
+
+struct EditFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditFoodView()
+    }
+}
