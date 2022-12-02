c4a2466f6e17d2e8d046646008aca9570ca3eb2c
Christopher Mata <91340066+Christopher-Mata@users.noreply.github.com>
2022-12-02 11:05:50 -0600

    Deleted a file that was not needed to be commited

--- iCalories/iCalories/DataModel/DataController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DataController.swift
-//  iCalories
-//
-//  Created by Christopher Mata on 8/26/22.
-//
-
-import Foundation
--- iCalories/iCalories/Helpers/TimeFormatting.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TimeFormatting.swift
-//  iCalories
-//
-//  Created by Christopher Mata on 8/26/22.
-//
-
-import Foundation
